6e033756f0efdce68aa9452697712f2c5f4fbc06
AviBoter <54690196+AviBoter@users.noreply.github.com>
2021-11-12 13:22:17 +0200

    Update ObjectMove.cs
    Stupid mistake fixed

--- Assets/Scripts/Objects/ObjectMove.cs
+++ Assets/Scripts/Objects/ObjectMove.cs
@@ -5,13 +5,18 @@ using System;
 //Apple's slow movement
 public class ObjectMove : MonoBehaviour 
 {
-    [Obsolete]
+    GameObject _GameManager;
+    void Start()
+	{
+		_GameManager = GameObject.FindObjectOfType<GameManagerRelated.GameManager>();
+	}
+[Obsolete]
     void FixedUpdate () 
 	{
 		transform.position = new Vector3(transform.position.x , transform.position.y - 0.05f, 0);
 		if (transform.position.y <= -4.5f)
 		{
-				GameObject.FindObjectOfType<GameManagerRelated.GameManager>().LostFood++;
+				_GameManager..LostFood++;
 				Destroy(gameObject);
 		}
 	}
